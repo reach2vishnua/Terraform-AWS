3a1fc877afdc48084a76b99d45976fcd8e9ac8e4
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-07 15:29:28 +0530

    Variables Example

---
+++ Variables/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.60.0"
+    }
+  }
+}
+provider "aws" {
+region = var.region  
+}
+resource "aws_security_group" "var_demo" {
+  name        = "vishnulabs-variables"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
+}
---
+++ Variables/variables.tf
@@ -0,0 +1,8 @@
+variable "vpn_ip" {
+    default = "116.50.30.50/32"
+}
+
+variable "region" {
+    default = "ap-south-1"
+  
+}

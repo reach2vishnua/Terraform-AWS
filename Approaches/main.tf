44cfa45a42645fad5620ad5ae9c548afa6ad8644
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-07 16:08:50 +0530

    Example for Approaches to Variable Assignments

---
+++ Approaches/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.60.0"
+    }
+  }
+}
+resource "aws_instance" "myec2" {
+   ami = "ami-041db4a969fe3eb68"
+   instance_type = var.instancetype
+}

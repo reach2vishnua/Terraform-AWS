6d36c10e35ff7bcb7252191414a1e6500b545457
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-07 16:44:26 +0530

    Data Types for Variables

---
+++ DataTypes/iam.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.60.0"
+    }
+  }
+}
+resource "aws_iam_user" "lb" {
+  name = var.usernumber
+  path = "/system/"
+}
---
+++ DataTypes/terraform.tfvars
@@ -0,0 +1,3 @@
+elb_name="vishnu"
+timeout="400"
+az=["ap-south-1a","ap-south-1b"]

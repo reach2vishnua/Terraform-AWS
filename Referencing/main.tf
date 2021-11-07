51ef8570f98fa485787d8ae4491aa3f88027491b
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-07 14:57:51 +0530

    Referencing Output Value to Another Resource

---
+++ Referencing/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.60.0"
+    }
+  }
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-041db4a969fe3eb68"
+   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "lb" {
+  vpc      = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myec2.id
+  allocation_id = aws_eip.lb.id
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "vishnulabs-security-group"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+
+#    cidr_blocks = [aws_eip.lb.public_ip/32]
+  }
+}

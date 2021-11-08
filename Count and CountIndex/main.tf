59b5023a6d4a8ef6b822a4a1a0491f6541e6790e
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-08 16:38:00 +0530

    Demo for Count and Count Index

--- Count and CountIndex/main.tf
+++ Count and CountIndex/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.60.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.60.0"
     }
   }
 }
 variable "elb_names" {
-    type = list
-    default = ["dev-loadbalancer","stage-loadbalancer","prod-loadbalancer"]
-  }
+  type    = list(any)
+  default = ["dev-loadbalancer", "stage-loadbalancer", "prod-loadbalancer"]
+}
 resource "aws_iam_user" "lb" {
-  name = var.elb_names[count.index]
-  path = "/system/"
+  name  = var.elb_names[count.index]
+  path  = "/system/"
   count = 3
 }

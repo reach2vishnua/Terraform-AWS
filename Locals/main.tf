905f8796e0adf9133f3f03c1b84b2c93238e10b7
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-08 16:41:14 +0530

    Demo for Local Values

--- Locals/main.tf
+++ Locals/main.tf
@@ -1,33 +1,33 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.60.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.60.0"
     }
   }
 }
 
 locals {
   common_tags = {
-      owner = "Devops Team"
-      service = "backend"
+    owner   = "Devops Team"
+    service = "backend"
   }
 }
 
 resource "aws_instance" "app-dev" {
-   ami = "ami-041db4a969fe3eb68"
-   instance_type = "t2.micro"
-   tags = local.common_tags
+  ami           = "ami-041db4a969fe3eb68"
+  instance_type = "t2.micro"
+  tags          = local.common_tags
 }
 
 resource "aws_instance" "db-dev" {
-   ami = "ami-041db4a969fe3eb68"
-   instance_type = "t2.small"
-   tags = local.common_tags
+  ami           = "ami-041db4a969fe3eb68"
+  instance_type = "t2.small"
+  tags          = local.common_tags
 }
 
 resource "aws_ebs_volume" "db_ebs" {
-   availability_zone = "ap-south-1a"
-   size = 8
-   tags = local.common_tags
+  availability_zone = "ap-south-1a"
+  size              = 8
+  tags              = local.common_tags
 }

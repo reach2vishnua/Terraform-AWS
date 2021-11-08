c0e0177afb14a8610208510811bb933fc18dd7b0
Avula VishnuVardhan Reddy <reach2vishnua@gmail.com>
2021-11-08 16:32:30 +0530

    MY First EC2 Instance

--- First-EC2-Instance/main.tf
+++ First-EC2-Instance/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-      aws = {
-          source = "hashicorp/aws"
-          version = "~> 3.0"
-      }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
 resource "aws_instance" "myec2" {
-    ami = "ami-041db4a969fe3eb68"
-    instance_type = "t2.nano"
-  }
+  ami           = "ami-041db4a969fe3eb68"
+  instance_type = "t2.nano"
+}
